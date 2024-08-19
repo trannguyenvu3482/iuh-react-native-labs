4a695f2f20a9e33f481647d9b65b0f82d486ee2e
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2024-08-19 14:04:05 +0700

    finish ex8 of lab1

---
+++ Lab01/JS_Coding_Challenges/ex8.js
@@ -0,0 +1,21 @@
+// Test data:
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = (bill) => {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
+}
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
