93cd2dd60236ff1eb0fc2f2a87d40d7c1c5db8de
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2024-08-26 13:42:41 +0700

    added array methods recreation

---
+++ ArrayMethods/arrayMethods.js
@@ -0,0 +1,76 @@
+// Array methods
+
+// Map
+Array.prototype.myMap = function (callback) {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+  return result;
+};
+
+// Filter
+Array.prototype.myFilter = function (callback) {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
+// Find
+Array.prototype.myFind = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return this[i];
+    }
+  }
+};
+
+// Reduce
+Array.prototype.myReduce = function (callback, initialValue) {
+  // Duplicate the array
+  const array = this;
+
+  // Check if initialValue is provided, if not, set the first element of the array as the initialValue
+  let accumulator = initialValue || array[0];
+
+  // Set the starting index accordingly
+  let i = initialValue ? 0 : 1;
+
+  // Loop through the array and apply the callback function
+  for (; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i], i, array);
+  }
+
+  return accumulator;
+};
+
+// Some
+Array.prototype.mySome = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Every
+Array.prototype.myEvery = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (!callback(this[i], i, this)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// forEach
+Array.prototype.myForEach = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
